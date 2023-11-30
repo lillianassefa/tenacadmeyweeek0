d174b06c08ead22176a64e5f91219630403aa986
lillianassefa <lillianalehegn123@gmail.com>
2023-11-30 16:36:51 +0300

    compeleted the user with highest number of reply count

--- src/utils.py
+++ src/utils.py
@@ -188,6 +188,22 @@ def get_tagged_users(df):
 
     return df['msg_content'].map(lambda x: re.findall(r'@U\w+', x))
 
+def get_community_participation(path_channel):
+    """ specify path to get json files"""
+    combined = []
+    comm_dict = {}
+    for json_file in glob.glob(f"{path_channel}*.json"):
+        with open(json_file, 'r') as slack_data:
+                combined.append(slack_data)
+        # print(f"Total json files is {len(combined)}")
+        for i in combined:
+            a = json.load(open(i.name, 'r', encoding='utf-8'))
+            for msg in a:
+                if 'replies' in msg.keys():
+                    for i in msg['replies']:
+                        comm_dict[i['user']] = comm_dict.get(i['user'], 0)+1
+    return comm_dict    
+
 
     
 def map_userid_2_realname(user_profile: dict, comm_dict: dict, plot=False):
